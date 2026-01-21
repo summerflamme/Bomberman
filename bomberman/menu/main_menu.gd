61963e2a77887dfc9549a57d13eb2e59cad65a30
Pierre Cornu <nsipierrecornu@gmail.com>
2026-01-21 10:11:09 +0100

    Ajout Menu de sélection du nombre de joueurs

--- bomberman/menu/main_menu.gd
+++ bomberman/menu/main_menu.gd
@@ -1,16 +1,15 @@
 extends Control
-@onready var game = preload("res://main.tscn")
+@onready var playerMenu = preload("res://menu/playerMenu.tscn")
 @onready var creditMenu = preload("res://menu/credit_menu.tscn")
 @onready var controlMenu = preload("res://menu/control.tscn")
 
 
 func _on_play_btn_button_down() -> void:
 	await get_tree().create_timer(0.5).timeout
-	get_tree().change_scene_to_packed(game)
+	get_tree().change_scene_to_packed(playerMenu)
 	
 func _on_button_button_down() -> void:
 	get_tree().change_scene_to_packed(creditMenu)
-
 
 func _on_control_btn_button_down() -> void:
 	get_tree().change_scene_to_packed(controlMenu)
